fe6b97fbdc5a94b767f0765bb42386f6e56f6ebb
Maxim Dalton <maximcdalton@gmail.com>
2018-05-30 16:12:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string) {
   else if (string.toLowerCase() === string) {
     return "I can't hear you!"
   }
-  else if (string.toUpperCase() === string {
+  else if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
 }
